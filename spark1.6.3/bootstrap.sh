71c18bb27619cea27d8ba7253127a67cc5799ee4
Brian <l.brian.hoang@gmail.com>
2017-11-17 10:36:08 -0800

    Added r version

--- spark1.6.3/bootstrap.sh
+++ spark1.6.3/bootstrap.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-IFS='.' read -r -a baseVersion << $R_VERSION
-curl -O https://cran.r-project.org/src/base/R-${baseVersion[0]}/R-$R_VERSION.tar.gz \
+IFS='.' read -r -a baseVersion << $1
+curl -O https://cran.r-project.org/src/base/R-${baseVersion[0]}/R-$1.tar.gz \
