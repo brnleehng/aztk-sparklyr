e7e863794938258fdc619d53d0a5d882a003551b
Brian <l.brian.hoang@gmail.com>
2017-11-17 11:44:32 -0800

    Fixed bash script

--- spark1.6.3/bootstrap.sh
+++ spark1.6.3/bootstrap.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-IFS='.' read -r -a baseVersion << $1
-curl -O https://cran.r-project.org/src/base/R-${baseVersion[0]}/R-$1.tar.gz \
+IFS='.' read majorVersion minorVersion patchVersion <<< $1
+curl -O https://cran.r-project.org/src/base/R-${majorVersion}/R-$1.tar.gz \
